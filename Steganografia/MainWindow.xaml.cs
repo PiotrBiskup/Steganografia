3d7a76847342a2f3610463b92d2a109fb21e41cb
PBiskup <p.biskup@expansio.pl>
2018-12-02 13:13:11 +0100

    Add project files.

---
+++ Steganografia/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Steganografia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BitmapImage bitmap;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            ofd.Title = "Open Image";
+            ofd.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.bmp, *.png) | *.jpg; *.jpeg; *.jpe; *.bmp; *.png"; ;
+
+            if (ofd.ShowDialog() == true)
+            {
+                bitmap = new BitmapImage();
+                bitmap.UriSource = new Uri(ofd.FileName);
+                
+            }
+
+            TestImg.Source = bitmap;
+
+        }
+
+        private void EncryptButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DecryptButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveOutputButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
